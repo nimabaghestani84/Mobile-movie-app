aaf25c140d09b6561e6c6b4d01e7f112f71db246
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-03 16:28:44 +0330

    delete test

--- movie-app/app/index.tsx
+++ movie-app/app/index.tsx
@@ -5,8 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl  font-bold text-primary">welcome</Text>
-      <Link href="/onBoarding">onBoarding</Link>
-      <Link href="/movie/avenger">avenger</Link>
     </View>
   );
 }
