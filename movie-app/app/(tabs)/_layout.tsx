7178eda90b58a9bd1330ab3e819aa144d55b15ba
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-06 14:43:47 +0330

    create TabIcon componenet

--- movie-app/app/(tabs)/_layout.tsx
+++ movie-app/app/(tabs)/_layout.tsx
@@ -4,7 +4,28 @@ import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 
-
+const TabIcon = ({ focused, title, icon }) => {
+  if (focused) {
+    return (
+      <>
+        <ImageBackground
+          source={images.highlight}
+          className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        >
+          <Image source={icon} tintColor="#151312" className="size-5" />
+          <Text className="text-secondary text-base font-semibold ml-2">
+            {title}
+          </Text>
+        </ImageBackground>
+      </>
+    );
+  }
+  return(
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+        <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  )
+};
 
 const _layout = () => {
   return (
