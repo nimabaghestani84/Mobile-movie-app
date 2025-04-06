1ad0516daeb3faac51cd4ee694c230c39dbd31dd
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-06 16:28:30 +0330

    end tab

--- movie-app/app/(tabs)/_layout.tsx
+++ movie-app/app/(tabs)/_layout.tsx
@@ -1,72 +1,87 @@
-import { View, Text, ImageBackground, Image } from "react-native";
-import React from "react";
 import { Tabs } from "expo-router";
-import { images } from "@/constants/images";
+import { ImageBackground, Image, Text, View } from "react-native";
 import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 
-const TabIcon = ({ focused, title, icon }:any) => {
-  if (focused) {
-    return (
-      <>
+function TabIcon({ focused, icon, title }: any) {
+  return (
+    <View
+      className="flex-1 items-center"
+      style={{
+        margin: focused ? 0 : 0,
+        padding: focused ? 0 : 0,
+        justifyContent: "center",
+      }}
+    >
+      {focused ? (
         <ImageBackground
           source={images.highlight}
-          className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+          className="flex-row items-center justify-center px-4 rounded-full h-12"
+          resizeMode="contain"
         >
-          <Image source={icon} tintColor="#151312" className="size-5" />
+          <Image source={icon} tintColor="#151312" className="w-5 h-5" />
           <Text className="text-secondary text-base font-semibold ml-2">
             {title}
           </Text>
         </ImageBackground>
-      </>
-    );
-  }
-  return(
-    <View className="size-full justify-center items-center mt-4 rounded-full">
-        <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+      ) : (
+        <Image source={icon} tintColor="#A8B5DB" className="w-5 h-5" />
+      )}
     </View>
-  )
-};
+  );
+}
 
-const _layout = () => {
+export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 30,
+          marginBottom: 36,
+          height: 70,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+        tabBarItemStyle: {
+          flex: 1,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
             <TabIcon focused={focused} icon={icons.home} title="Home" />
           ),
         }}
       />
-
       <Tabs.Screen
         name="search"
         options={{
-          title: "Search",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
             <TabIcon focused={focused} icon={icons.search} title="Search" />
           ),
         }}
       />
-
       <Tabs.Screen
         name="saved"
         options={{
-          title: "Saved",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
             <TabIcon focused={focused} icon={icons.save} title="Saved" />
           ),
         }}
       />
-
       <Tabs.Screen
         name="profile"
         options={{
-          title: "Profile",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
             <TabIcon focused={focused} icon={icons.person} title="Profile" />
@@ -75,6 +90,4 @@ const _layout = () => {
       />
     </Tabs>
   );
-};
-
-export default _layout;
+}
