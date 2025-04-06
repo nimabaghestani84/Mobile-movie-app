766083f1968427280a461b144a6e30c5ec6cf9f6
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-06 14:42:48 +0330

    custom icon tabs

--- movie-app/app/(tabs)/_layout.tsx
+++ movie-app/app/(tabs)/_layout.tsx
@@ -1,31 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text, ImageBackground, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+
+
 
 const _layout = () => {
   return (
-   <Tabs>
-    <Tabs.Screen
-    name='index'
-    options={{title:'Home',headerShown:false}}
-    />
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
 
-<Tabs.Screen
-    name='search'
-    options={{title:'Search',headerShown:false}}
-    />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Search" />
+          ),
+        }}
+      />
 
-<Tabs.Screen
-    name='saved'
-    options={{title:'Saved',headerShown:false}}
-    />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} title="Saved" />
+          ),
+        }}
+      />
 
-<Tabs.Screen
-    name='profile'
-    options={{title:'Profile',headerShown:false}}
-    />
-   </Tabs>
-  )
-}
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
 
-export default _layout
+export default _layout;
