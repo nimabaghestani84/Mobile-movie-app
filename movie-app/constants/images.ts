723cd3b22f7c3959bf66a8b3f72511176ea9fdcc
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-12 20:10:45 +0330

    Update images.ts

--- movie-app/constants/images.ts
+++ movie-app/constants/images.ts
@@ -1,9 +1,9 @@
-// import bg from "../assets/images/bg.png";
+import bg from "../assets/images/bg.png";
 import highlight from "../assets/images/highlight.png";
 import rankingGradient from "../assets/images/rankingGradient.png";
 
 export const images = {
-  // bg,
+  bg,
   highlight,
   rankingGradient,
 };
