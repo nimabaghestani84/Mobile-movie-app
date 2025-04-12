8153c1430e326a83a1216436c14e84052d4c71aa
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-12 20:46:37 +0330

    Create api.ts

---
+++ movie-app/services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzIyNjEwZThkZjk2MTAyN2U5YWIxYzhmMWZmNzZlNiIsIm5iZiI6MTc0NDQ3Njc2Mi4zMDQsInN1YiI6IjY3ZmE5YTVhYzc0YzE2YmY4OGFkMDAyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5DRiA8n72QpiNygruiF8bm35dsWtdsF81hWZEfQHoeM'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
