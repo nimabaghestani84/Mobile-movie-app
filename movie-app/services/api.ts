b7e9a2f9e6347cbd5670983ebe8b10a23a5145d5
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-13 20:11:22 +0330

    Update api.ts

--- movie-app/services/api.ts
+++ movie-app/services/api.ts
@@ -1,13 +1,10 @@
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzIyNjEwZThkZjk2MTAyN2U5YWIxYzhmMWZmNzZlNiIsIm5iZiI6MTc0NDQ3Njc2Mi4zMDQsInN1YiI6IjY3ZmE5YTVhYzc0YzE2YmY4OGFkMDAyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5DRiA8n72QpiNygruiF8bm35dsWtdsF81hWZEfQHoeM'
-//   }
-// };
+export const TMDB_CONFIG={
+    BASE_URL:"https://api.themoviedb.org/3",
+    API_KEY:process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers:{
+        accept:'application/json',
+        Authorization:`Bearer:${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
 
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
+// discover/movie
