d01b770823fbb1700358795a1f87d43856251e99
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-17 15:35:22 +0330

    Create MovieCard.tsx

---
+++ movie-app/Components/MovieCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <View>
+      <Text className='text-white text-sm'>MovieCard</Text>
+    </View>
+  )
+}
+
+export default MovieCard
