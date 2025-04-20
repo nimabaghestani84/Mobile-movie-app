2812dd6b6af465d3925c5bef998b2e8676b4b56f
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-20 13:45:40 +0330

    create bg of page

--- movie-app/app/(tabs)/search.tsx
+++ movie-app/app/(tabs)/search.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, FlatList } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+import MovieCard from "@/Components/MovieCard";
+import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 const search = () => {
   return (
-    <View>
-      <Text>search</Text>
-    </View>
-  )
-}
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
 
-export default search
+     
+    </View>
+  );
+};
+
+export default search;
