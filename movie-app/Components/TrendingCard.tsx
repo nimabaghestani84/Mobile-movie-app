e36181b895ec7480aecd7b34dc18ea861cebb912
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-27 20:36:15 +0330

    props

--- movie-app/Components/TrendingCard.tsx
+++ movie-app/Components/TrendingCard.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const TrendingCard = () => {
+const TrendingCard = ({
+    movie: { movie_id, title, poster_url },
+    index,
+  }: TrendingCardProps) => {
   return (
     <View>
       <Text>TrendingCard</Text>
