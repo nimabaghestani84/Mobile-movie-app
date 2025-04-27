5acfd1b8e3932917cc3f35729cd807fc6985e932
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-27 20:02:40 +0330

    Update index.tsx

--- movie-app/app/(tabs)/index.tsx
+++ movie-app/app/(tabs)/index.tsx
@@ -14,9 +14,16 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/Components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -34,34 +41,38 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto " />
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error:{moviesError?.message}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error:{moviesError?.message || trendingError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
               onPress={() => router.push("/search")}
               placeholder="Search for a movie"
             />
+
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white mb-3">Trending Movies</Text>
+              </View>
+            )}
           </View>
         )}
         <View className="flex-1 mt-5">
-
           <Text className="text-lg text-white font-bold mt-5 mb-3">
             Latest Movies
           </Text>
+
+          <FlatList className="mb-4 mt-3" data={trendingMovies} renderItem={({item,index})=>(<Text className="text-white text-sm">{item.title}</Text>)}/>
+
           <FlatList
             data={movies}
-            renderItem={({ item }) => (
-              <MovieCard
-                {...item}
-              />
-            )}
+            renderItem={({ item }) => <MovieCard {...item} />}
             keyExtractor={(item) => item.id.toString()}
             numColumns={3}
             columnWrapperStyle={{
