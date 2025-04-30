ba1b653c7ab8f2f071d2bc31a4a63e12ceba44fa
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-30 19:35:39 +0330

    Update TrendingCard.tsx

--- movie-app/Components/TrendingCard.tsx
+++ movie-app/Components/TrendingCard.tsx
@@ -14,6 +14,10 @@ const TrendingCard = ({
           className="w-32 h-48 rounded-lg"
           resizeMode="cover"
         />
+
+        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+
+        </View>
       </TouchableOpacity>
     </Link>
   );
