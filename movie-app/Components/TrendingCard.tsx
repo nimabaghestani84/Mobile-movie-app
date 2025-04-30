c4824f55f6efa99a04503a212d486b4b583a358d
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-04-30 19:49:54 +0330

    star image

--- movie-app/Components/TrendingCard.tsx
+++ movie-app/Components/TrendingCard.tsx
@@ -1,6 +1,8 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
 import React from "react";
 import { Link } from "expo-router";
+import { images } from "@/constants/images";
 
 const TrendingCard = ({
   movie: { movie_id, title, poster_url },
@@ -16,7 +18,9 @@ const TrendingCard = ({
         />
 
         <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
-
+          <MaskedView maskElement={<Text className="font-bold text-white text-6xl">{index + 1}</Text>}>
+          <Image source={images.rankingGradient} className="size-14" resizeMode="cover"/>
+          </MaskedView>
         </View>
       </TouchableOpacity>
     </Link>
