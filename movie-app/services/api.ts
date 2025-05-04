9dd171e2861612f5f10f483cd1cfd85451c93ff0
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-04 12:52:54 +0330

    condition for response

--- movie-app/services/api.ts
+++ movie-app/services/api.ts
@@ -41,7 +41,9 @@ export const fetchMovieDetails = async (
       }
     );
 
-   
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+    }
 
     const data = await response.json();
     return data;
