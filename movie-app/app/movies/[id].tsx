eb774f81a94b103feed779d49c98e3a383f12af9
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-04 13:03:00 +0330

    title and time

--- movie-app/app/movies/[id].tsx
+++ movie-app/app/movies/[id].tsx
@@ -13,6 +13,14 @@ const MovieDetails = () => {
         <View>
           <Image className="w-full h-[550px]" resizeMode="stretch" source={{uri:`https://image.tmbd.org/t/p/w500${movie?.poster_path}`}} />
         </View>
+
+        <View className="flex-col items-start justify-center mt-5 px-5">
+            <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+            <View className="flex-row items-center gap-x-1 mt-2">
+                <Text className="text-light-200 text-sm">{movie?.release_date?.split('-')[0]}</Text>
+                <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
+            </View>
+        </View>
       </ScrollView>
     </View>
   );
