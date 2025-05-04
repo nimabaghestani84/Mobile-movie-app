4c7c208f3c08086af485fcd451dcc64be79ea3c3
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-04 12:57:54 +0330

    fetch by id from useFetch

--- movie-app/app/movies/[id].tsx
+++ movie-app/app/movies/[id].tsx
@@ -1,11 +1,18 @@
 import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovieDetails } from "@/services/api";
 
 const MovieDetails = () => {
+  const {id}=useLocalSearchParams();
+  const {data:movie,loading}=useFetch(()=>fetchMovieDetails(id as string))
   return (
     <View className="bg-primary flex-1">
       <ScrollView contentContainerStyle={{paddingBottom:80}}>
-        
+        <View>
+          <Image source={{uri:`https://image.tmbd.org/t/p/w500${movie?.poster_path}`}} />
+        </View>
       </ScrollView>
     </View>
   );
