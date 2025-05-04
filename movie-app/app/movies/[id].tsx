67075b969d42e8480cc3a2ae892855521ee95e52
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-04 14:53:48 +0330

    Update [id].tsx

--- movie-app/app/movies/[id].tsx
+++ movie-app/app/movies/[id].tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useLocalSearchParams } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovieDetails } from "@/services/api";
+import { icons } from "@/constants/icons";
 
 const MovieDetails = () => {
   const {id}=useLocalSearchParams();
@@ -19,6 +20,14 @@ const MovieDetails = () => {
             <View className="flex-row items-center gap-x-1 mt-2">
                 <Text className="text-light-200 text-sm">{movie?.release_date?.split('-')[0]}</Text>
                 <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
+            </View>
+
+            <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+              <Image source={icons.star} className="size-4"/>
+              <Text className="text-white font-bold text-sm">{Math.round(movie?.vote_average??0)}/10</Text>
+              <Text className="text-light-200 text-sm">
+              ({movie?.vote_count} votes)
+            </Text>
             </View>
         </View>
       </ScrollView>
