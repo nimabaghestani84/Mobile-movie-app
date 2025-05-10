2b846e7109618a92f3f33b40d2d0d397d985c7af
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-10 18:40:12 +0330

    import icon

--- movie-app/app/(tabs)/saved.tsx
+++ movie-app/app/(tabs)/saved.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { icons } from '@/constants/icons'
 
 const saved = () => {
   return (
    <View className='bg-primary flex-1 px-10'>
     <View className='flex justify-center items-center flex-1 flex-col gap-5'>
-      
+      <Image source={icons.person} className='size-10' tintColor="#fff"/>
     </View>
    </View>
   )
