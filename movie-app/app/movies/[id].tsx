db67121f859d4ff49fc8c7b8706c95c12f5cb815
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-10 18:33:18 +0330

    create TouchableOpacity for back

--- movie-app/app/movies/[id].tsx
+++ movie-app/app/movies/[id].tsx
@@ -53,6 +53,13 @@ const MovieDetails = () => {
               <MovieInfo label="Production Companies" value={movie?.production_companies.map((c)=>c.name).join(' - ')|| 'N/A'}/>
         </View>
       </ScrollView>
+
+      <TouchableOpacity className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50">
+        <Image source={icons.arrow} className="size-5 mr-1 mt-0.5 rotate-180" tintColor="#fff"/>
+        <Text className="text-white font-semibold text-base">
+          Go Back
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
