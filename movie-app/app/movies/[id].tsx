054256163a14c25343a57670d1c7d660b20896c6
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-10 18:34:08 +0330

    Update [id].tsx

--- movie-app/app/movies/[id].tsx
+++ movie-app/app/movies/[id].tsx
@@ -1,6 +1,6 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovieDetails } from "@/services/api";
 import { icons } from "@/constants/icons";
@@ -54,7 +54,7 @@ const MovieDetails = () => {
         </View>
       </ScrollView>
 
-      <TouchableOpacity className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50">
+      <TouchableOpacity className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-50" onPress={router.back}>
         <Image source={icons.arrow} className="size-5 mr-1 mt-0.5 rotate-180" tintColor="#fff"/>
         <Text className="text-white font-semibold text-base">
           Go Back
