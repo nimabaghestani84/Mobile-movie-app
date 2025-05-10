781fbdbf1b69ae45ad3a93cd0711118c2fbb6fee
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-10 18:40:45 +0330

    Update saved.tsx

--- movie-app/app/(tabs)/saved.tsx
+++ movie-app/app/(tabs)/saved.tsx
@@ -7,6 +7,7 @@ const saved = () => {
    <View className='bg-primary flex-1 px-10'>
     <View className='flex justify-center items-center flex-1 flex-col gap-5'>
       <Image source={icons.save} className='size-10' tintColor="#fff"/>
+      <Text className='text-gray-500 text-base'>Profile</Text>
     </View>
    </View>
   )
