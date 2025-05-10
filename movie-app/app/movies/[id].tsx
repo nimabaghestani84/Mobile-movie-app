ec4906a15e51de361ca137d327eddc0d899ce4b1
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-10 18:44:34 +0330

    debog and end

--- movie-app/app/movies/[id].tsx
+++ movie-app/app/movies/[id].tsx
@@ -1,9 +1,16 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
-import React from "react";
-import { router, useLocalSearchParams } from "expo-router";
-import useFetch from "@/services/useFetch";
-import { fetchMovieDetails } from "@/services/api";
+import {
+  View,
+  Text,
+  Image,
+  ActivityIndicator,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { icons } from "@/constants/icons";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
 
 interface MovieInfoProps {
   label: string;
@@ -19,38 +26,58 @@ const MovieInfo = ({ label, value }: MovieInfoProps) => (
   </View>
 );
 
-const MovieDetails = () => {
+const Details = () => {
+  const router = useRouter();
   const { id } = useLocalSearchParams();
+
   const { data: movie, loading } = useFetch(() =>
     fetchMovieDetails(id as string)
   );
+
+  if (loading)
+    return (
+      <SafeAreaView className="bg-primary flex-1">
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+
   return (
     <View className="bg-primary flex-1">
       <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
         <View>
           <Image
-            className="w-full h-[550px]"
-            resizeMode="stretch"
             source={{
               uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
             }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
           />
+
+          <TouchableOpacity className="absolute bottom-5 right-5 rounded-full size-14 bg-white flex items-center justify-center">
+            <Image
+              source={icons.play}
+              className="w-6 h-7 ml-1"
+              resizeMode="stretch"
+            />
+          </TouchableOpacity>
         </View>
 
         <View className="flex-col items-start justify-center mt-5 px-5">
           <Text className="text-white font-bold text-xl">{movie?.title}</Text>
           <View className="flex-row items-center gap-x-1 mt-2">
             <Text className="text-light-200 text-sm">
-              {movie?.release_date?.split("-")[0]}
+              {movie?.release_date?.split("-")[0]} •
             </Text>
             <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
           </View>
 
           <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
             <Image source={icons.star} className="size-4" />
+
             <Text className="text-white font-bold text-sm">
               {Math.round(movie?.vote_average ?? 0)}/10
             </Text>
+
             <Text className="text-light-200 text-sm">
               ({movie?.vote_count} votes)
             </Text>
@@ -59,22 +86,26 @@ const MovieDetails = () => {
           <MovieInfo label="Overview" value={movie?.overview} />
           <MovieInfo
             label="Genres"
-            value={movie?.genres?.map((g) => g.name).join(" - ") || "N/A"}
+            value={movie?.genres?.map((g) => g.name).join(" • ") || "N/A"}
           />
+
           <View className="flex flex-row justify-between w-1/2">
             <MovieInfo
               label="Budget"
-              value={`$${movie?.budget / 1_000_000} million`}
+              value={`$${(movie?.budget ?? 0) / 1_000_000} million`}
             />
             <MovieInfo
               label="Revenue"
-              value={`$${Math.round(movie?.revenue / 1_000_000)}`}
+              value={`$${Math.round(
+                (movie?.revenue ?? 0) / 1_000_000
+              )} million`}
             />
           </View>
+
           <MovieInfo
             label="Production Companies"
             value={
-              movie?.production_companies.map((c) => c.name).join(" - ") ||
+              movie?.production_companies?.map((c) => c.name).join(" • ") ||
               "N/A"
             }
           />
@@ -96,4 +127,4 @@ const MovieDetails = () => {
   );
 };
 
-export default MovieDetails;
+export default Details;
