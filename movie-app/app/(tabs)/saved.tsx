2287edf97d43d6ca9f9bd0293bb6e720543a2fb5
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-10 18:39:20 +0330

    section for icon

--- movie-app/app/(tabs)/saved.tsx
+++ movie-app/app/(tabs)/saved.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 const saved = () => {
   return (
    <View className='bg-primary flex-1 px-10'>
-    
+    <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+      
+    </View>
    </View>
   )
 }
