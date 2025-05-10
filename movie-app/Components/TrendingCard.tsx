7551e50c5629d6a4f8e83ba66573395315faa799
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2025-05-10 18:44:22 +0330

    end

--- movie-app/Components/TrendingCard.tsx
+++ movie-app/Components/TrendingCard.tsx
@@ -18,11 +18,22 @@ const TrendingCard = ({
         />
 
         <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
-          <MaskedView maskElement={<Text className="font-bold text-white text-6xl">{index + 1}</Text>}>
-          <Image source={images.rankingGradient} className="size-14" resizeMode="cover"/>
+          <MaskedView
+            maskElement={
+              <Text className="font-bold text-white text-6xl">{index + 1}</Text>
+            }
+          >
+            <Image
+              source={images.rankingGradient}
+              className="size-14"
+              resizeMode="cover"
+            />
           </MaskedView>
         </View>
-        <Text className="text-sm font-bold mt-2 text-light-200" numberOfLines={2}></Text>
+        <Text
+          className="text-sm font-bold mt-2 text-light-200"
+          numberOfLines={2}
+        ></Text>
       </TouchableOpacity>
     </Link>
   );
